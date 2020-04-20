3ddd17e30a0a2d84e7783f474b0ec172f2e174f1
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-04-20 19:31:56 +0530

    added limits-service,configuration.

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsServcieApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServcieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServcieApplication.class, args);
+	}
+
+}
