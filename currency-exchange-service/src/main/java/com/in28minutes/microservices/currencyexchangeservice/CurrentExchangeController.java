ebea910e4555ccb261057d9a06f173494a516305
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-04-22 22:03:14 +0530

    Adding the port as the attribute in the response json. Retrieving the port of the server from environment properties.

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrentExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrentExchangeController.java
@@ -1,8 +1,11 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import javax.websocket.server.PathParam;
 import java.math.BigDecimal;
@@ -10,8 +13,14 @@ import java.math.BigDecimal;
 @RestController
 public class CurrentExchangeController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to){
-        return new ExchangeValue(1000L,"usd","inr", BigDecimal.valueOf(65));
+
+        ExchangeValue exchangeValue = new ExchangeValue(1000L,"usd","inr", BigDecimal.valueOf(65));
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        return exchangeValue;
     }
 }
--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -6,7 +6,9 @@ public class ExchangeValue {
     private Long id;
     private String from;
     private String to;
+
     private BigDecimal conversionMultiple;
+    private int port;
 
     public Long getId() {
         return id;
@@ -38,6 +40,14 @@ public class ExchangeValue {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 
     public ExchangeValue() {
