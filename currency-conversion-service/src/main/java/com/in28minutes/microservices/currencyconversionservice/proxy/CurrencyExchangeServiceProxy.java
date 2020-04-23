2e8d1c17ec231b6cb10812d1b92868004a3e885e
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-04-23 23:23:02 +0530

    connecting to multiple instances(8000,8001) of currency-exchange-service from currency-conversion-service with Ribbon

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
