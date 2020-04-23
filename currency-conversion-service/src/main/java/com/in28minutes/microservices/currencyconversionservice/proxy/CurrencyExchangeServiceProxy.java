5c94f545ff1fbb64cc60e39fb0c98dd426daa96d
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-04-23 23:16:30 +0530

    reverting ribbon

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -7,8 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service",url="localhost:8000")
-@RibbonClient(name = "currency-exchange-service")
+@FeignClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
