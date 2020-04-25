80741978b112159ffeb0166a23319de6f01878b7
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-04-25 10:36:20 +0530

    Enabling spring cloud bus and Hystrix.

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package com.in28minutes.microservices.limitsservice.controller;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
 import com.in28minutes.microservices.limitsservice.configuration.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,15 @@ public class LimitsConfigurationController {
     @GetMapping("limits")
     public LimitConfiguration retrieveLimitsFromConfigurations(){
         return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration(){
+            throw new RuntimeException("Not Available.");
+    }
+
+    public  LimitConfiguration fallbackRetrieveConfiguration(){
+        return new LimitConfiguration(999,9);
     }
 }
